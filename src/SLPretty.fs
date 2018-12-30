95c3813858361fa5f1af18bd583b23a17860ed81
stephentetley <stephen.tetley@gmail.com>
2018-12-30 19:28:08 +0000

    Fleshing out the library.

--- src/SLPretty.fs
+++ src/SLPretty.fs
@@ -12,45 +12,27 @@ module SLPretty =
         printfn "Hello %s" name
     
     type Doc = 
-        | Nil
-        | Cat of Doc * Doc
-        | Nest of int * Doc
-        | Label of string * Doc
-        | Markup of unit * Doc        // TODO
-        | Text of string
-        | Line of bool
-        | Group of Doc
-        | Column of (int -> Doc)
-        | Nesting of (int -> Doc)   
+        private 
+            | Nil
+            | Cat of Doc * Doc
+            | Nest of int * Doc
+            | Label of string * Doc
+            | Markup of unit * Doc        // TODO
+            | Text of string
+            | Line of bool
+            | Group of Doc
+            | Column of (int -> Doc)
+            | Nesting of (int -> Doc)   
 
     type SimpleDoc = 
-        | SEmpty 
-        | SText of string * SimpleDoc
-        | SPush of unit * SimpleDoc      // TODO
-        | SPop of SimpleDoc              // TODO
-        | SLine of string * SimpleDoc      // TODO
+        private 
+            | SEmpty 
+            | SText of string * SimpleDoc
+            | SPush of unit * SimpleDoc      // TODO
+            | SPop of SimpleDoc              
+            | SLine of string * SimpleDoc     
 
 
-
-    let line : Doc = Line false
-
-    // break is reserved
-    let docbreak : Doc = Line true
-
-    let softLine : Doc = Group line
-
-    let softBreak : Doc = Group docbreak
-
-    let empty : Doc = Nil
-
-    let nest (i:int) (d:Doc) : Doc = Nest (i,d)
-
-    let text (s:string) : Doc = Text s 
-
-    let character (ch:char) : Doc = 
-        match ch with
-        | '\n' -> line 
-        | _ -> text <| ch.ToString()
 
 
     let extendString (s:string) (spaces:int) = 
@@ -72,7 +54,7 @@ module SLPretty =
         work document
 
 
-    let isTooBig (text:string) (col:int) (width:int) : bool = 
+    let private isTooBig (text:string) (col:int) (width:int) : bool = 
         col + text.Length > width
 
     type private LayoutAns = 
@@ -81,7 +63,7 @@ module SLPretty =
 
     exception ErrBacktrack
 
-    let layout (width:int) (doc:Doc) : SimpleDoc = 
+    let private layout (width:int) (doc:Doc) : SimpleDoc = 
         let rec best (col:int) (docs: list<LayoutAns * Doc>) (alternate:bool) =
             match docs with
             | [] -> SEmpty
@@ -99,14 +81,19 @@ module SLPretty =
                 SPush(f, best col ((iz, x) :: (False, Nil) :: rest) alternate) 
             | (String iz, Line _) :: rest -> 
                 SLine(iz, best iz.Length rest alternate)
-            | (iz,Text(t)) :: rest ->
+            | (iz, Group(x)) :: rest ->
+                try
+                    best col ((iz, flatten x) :: rest) alternate
+                with
+                | ErrBacktrack -> best col ((iz, x) :: rest) alternate
+            | (iz, Text(t)) :: rest ->
                 if (width >= 0) && alternate && isTooBig t col width then
                     raise ErrBacktrack
                 else
                     SText(t, best (col + t.Length) rest alternate)
-            | (iz,Column(f)) :: rest ->
+            | (iz, Column(f)) :: rest ->
                 best col ((iz, f col) :: rest) alternate
-            | (String iz,Nesting(f)) :: rest ->
+            | (String iz, Nesting(f)) :: rest ->
                 best col ((String iz, f iz.Length) :: rest) alternate
         best 0 [(String "",doc)] false
 
@@ -129,3 +116,163 @@ module SLPretty =
 
         print (layout width doc)
         sb.ToString()
+
+    
+
+
+    // ************************************************************************
+    // Primitive printers   
+
+    let empty : Doc = Nil
+    
+    let nest (i:int) (d:Doc) : Doc = Nest (i,d)
+    
+    let text (s:string) : Doc = Text s 
+
+    let label (l:string) (d:Doc) : Doc = Label(l,d)
+
+    let markup f (d:Doc) : Doc = Markup(f,d)
+
+    let column (f:int -> Doc) : Doc = Column(f)
+
+    let nesting (f:int -> Doc) : Doc = Nesting(f)
+
+    let group (d:Doc) : Doc = Group(d)
+
+    let line : Doc = Line false
+    
+    let linebreak : Doc = Line true
+
+    let character (ch:char) : Doc = 
+        match ch with
+        | '\n' -> line 
+        | _ -> text <| ch.ToString()
+
+
+    let softline : Doc = Group line
+
+    let softbreak : Doc = Group linebreak
+
+    
+    
+
+    
+
+    let beside (x:Doc) (y:Doc) : Doc = Cat(x,y)
+
+    // Don't try to define (<>) - it is a reserved operator name in F#
+
+
+    // aka beside
+    let (^^) (x:Doc) (y:Doc) = beside x y
+
+    let besideSpace (x:Doc) (y:Doc) : Doc = x ^^ character ' ' ^^ y
+
+    let (^+^) (x:Doc) (y:Doc) : Doc = besideSpace x y
+
+    let (^@^) (x:Doc) (y:Doc) : Doc = x ^^ line ^^ y
+    let (^@@^) (x:Doc) (y:Doc) : Doc = x ^^ linebreak ^^ y
+
+    // ************************************************************************
+    // Character printers
+
+    /// Single left parenthesis: '('
+    let lparen : Doc = character '('
+
+    /// Single right parenthesis: ')'
+    let rparen : Doc = character ')'
+
+    /// Single left angle: '<'
+    let langle : Doc = character '<'
+
+    /// Single right angle: '>'
+    let rangle : Doc = character '>'
+
+    /// Single left brace: '{'
+    let lbrace : Doc = character '{'
+    
+    /// Single right brace: '}'
+    let rbrace : Doc= character '}'
+    
+    /// Single left square bracket: '['
+    let lbracket : Doc = character '['
+    
+    /// Single right square bracket: ']'
+    let rbracket : Doc = character ']'
+
+
+    /// Single quote: '
+    let squote : Doc= character '\''
+
+    ///The document @dquote@ contains a double quote, '\"'.
+    let dquote : Doc = character '"'
+
+    /// The document @semi@ contains a semi colon, \";\".
+    let semi : Doc = character ';'
+
+    /// The document @colon@ contains a colon, \":\".
+    let colon : Doc = character ':'
+
+    /// The document @comma@ contains a comma, \",\".
+    let comma : Doc = character ','
+
+    /// The document @space@ contains a single space, \" \".
+    let space : Doc = character ' '
+
+    /// The document @dot@ contains a single dot, \".\".
+    let dot : Doc = character '.'
+
+    /// The document @backslash@ contains a back slash, \"\\\".
+    let backslash : Doc = character '\\'
+
+    /// The document @equals@ contains an equal sign, \"=\".
+    let equals : Doc = character '='
+
+
+    let spaces (i:int) : Doc = text <| String.replicate i " "
+
+    // ************************************************************************
+    // List concatenation 
+
+    let foldDocs (op:Doc -> Doc -> Doc) (docs:Doc list) : Doc = 
+        match docs with
+        | [] -> empty
+        | (x::xs) -> List.fold op x xs
+
+    let punctuate (sep:Doc) (docs:Doc list) : Doc =
+        let rec work acc ds =
+            match ds with
+            | [] -> acc
+            | (x :: xs) -> work (Cat(acc, Cat(sep,x))) xs
+        match docs with
+        | [] -> empty
+        | (x :: xs) -> work x xs
+
+    
+    let hcat (docs:Doc list) : Doc = foldDocs beside docs
+
+    let hcatSpace (docs:Doc list) : Doc = punctuate space docs
+
+    let vcat (docs:Doc list) : Doc = punctuate line docs
+
+    let vcatSoft (docs:Doc list) : Doc = punctuate softline docs
+
+    let vcatSoftBreak (docs:Doc list) : Doc = punctuate softbreak docs
+
+
+    let width d f = 
+        column (fun k1 -> d ^^ column (fun k2 -> f (k2 - k1)) )
+
+    let align (d:Doc) = 
+        column (fun k -> nesting (fun i -> nest (k - i) d))
+
+    let hang (i:int) (d:Doc) : Doc = align (nest i d)
+
+    let indent (i:int) (d:Doc) : Doc = 
+        hang i (spaces i ^^ d)
+
+    let fill f d = 
+        width d (fun w -> if w >= f then empty else spaces (f - w))
+
+    let fillBreak f d = 
+        width d (fun w -> if w > f then nest f linebreak else spaces (f - w))
--- test/Test01.fsx
+++ test/Test01.fsx
@@ -11,3 +11,21 @@ let test01 () =
 
 let test02 () = 
     prettyPrint (text "hello") 80
+
+let testNesting01 () : unit = 
+    let a : Doc = text "aaa"
+    let b : Doc = text "bbb"
+    let c : Doc = text "ccc"
+    let d : Doc = text "ddd"
+    let doc1 : Doc  = a ^@@^ (nest 2 (b ^@@^ c)) ^@@^ d
+    printfn "%s" <| prettyPrint doc1 80
+
+
+let testIndent01 () : unit = 
+    let a : Doc = text "aaa"
+    let b : Doc = text "bbb"
+    let c : Doc = text "ccc"
+    let d : Doc = text "ddd"
+    let doc1 : Doc  = a ^@@^ (indent 2 (b ^@@^ c)) ^@@^ d
+    printfn "%s" <| prettyPrint doc1 80
+
